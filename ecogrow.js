82caac624a5cdb89b3d952349c0d19cc2b8ad1bc
shreyo2005 <diya.laik2005@gmail.com>
2025-01-19 12:36:42 +0530

    Update ecogrow.js

--- ecogrow.js
+++ ecogrow.js
@@ -28,6 +28,30 @@ tl.from("h1",{
     y:-30,
     opacity:0,
     duration:1,
+});
+    gsap.to("#soil1",{
+    x:-5,
+    delay:2,
+    duration:1,
+    repeat:-1,
+    yoyo:true
+});
+gsap.to("#tools1",{
+    x:-5,
+    delay:2,
+    duration:1,
+    repeat:-1,
+    yoyo:true
+});
+gsap.to("#seeds1",{
+    x:-5,
+    delay:2,
+    duration:1,
+    repeat:-1,
+    yoyo:true
+    
+});
+
     
     
 })
